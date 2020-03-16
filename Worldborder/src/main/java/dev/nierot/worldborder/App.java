0d03afdaa19231b0c233812e953d5656444bb091
Niels <nielsrotmensen@outlook.com>
2020-03-16 19:11:26 +0100

    Added the db connection

--- Worldborder/src/main/java/dev/nierot/worldborder/App.java
+++ Worldborder/src/main/java/dev/nierot/worldborder/App.java
@@ -1,6 +1,16 @@
 package dev.nierot.worldborder;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
@@ -9,9 +19,53 @@ import org.bukkit.WorldBorder;
 
 public class App extends JavaPlugin {
 
+    private Connection connection;
+    private Statement statement;
+    private String host, database, username, password, port;
+    private int size;
+    private File file;
+    private FileWriter writer;
+    
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Event(), this);
+        getServer().getPluginManager().registerEvents(new Event(this), this);
+        host = db.host;
+        database = db.database;
+        username = db.username;
+        password = db.password;
+        port = Integer.toString(db.port);
+        try {    
+            openConnection();
+            statement = connection.createStatement();          
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.size = getSize();
+        } catch (Exception e) {
+            Bukkit.broadcastMessage("MariaDB doet nait");
+        }
+    }
+
+    public Statement getStatement() {
+        return this.statement;
+    }
+
+    public void openConnection() throws SQLException, ClassNotFoundException {
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+     
+        synchronized (this) {
+            if (connection != null && !connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + this.host+ ":" + this.port + "/" + this.database, this.username, this.password);
+        }
     }
 
     @Override
@@ -31,9 +85,11 @@ public class App extends JavaPlugin {
                 } else if (args.length == 2) {
                     border.setSize(Integer.parseInt(args[1]));
                     sender.sendMessage("Set the worldborder to size: " + args[1]);
+                    updateSize(Integer.parseInt(args[1]));
                 } else if (args.length == 3) {
                     border.setSize(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
                     sender.sendMessage("Set the worldborder to size: " + args[1]);
+                    updateSize(Integer.parseInt(args[1]));
                 }
             return true;
             } else if (args[0].equals("getsize")) {
@@ -48,10 +104,58 @@ public class App extends JavaPlugin {
                     border.setCenter(Double.parseDouble(args[1]),Double.parseDouble(args[2]));
                     border.setSize(Double.parseDouble(args[3]));
                     Bukkit.broadcastMessage("A new border is created at coordinates: " + args[1] + ", " + args[2] + " with size: " + args[3]);
+                    updateSize(Integer.parseInt(args[1]),1);
                     return true;
                 }
+            } else if (args[0].equals("dbtest")) {
+                dbTest();
+                return true;
             }
         }
         return false;
+    }
+
+    public void dbTest() {
+        try {
+            ResultSet result = statement.executeQuery("SELECT * FROM mc.border");
+            while (result.next()) {
+                String name = result.getString("size");
+                Bukkit.broadcastMessage(name);
+            }
+        } catch (SQLException e) {
+            Bukkit.broadcastMessage(e.toString());
+        } catch (Exception f) {
+            Bukkit.broadcastMessage("Something went wrong");
+        }
+    }
+
+    public int getSize() {
+        try {
+            ResultSet res = statement.executeQuery("SELECT size FROM mc.size");
+            int size = Integer.parseInt(res.getString("size"));
+            this.size = size;
+        } catch (SQLException e) {
+            Bukkit.broadcastMessage(e.toString());
+        } catch (Exception f) {
+            Bukkit.broadcastMessage("Something went wrong");
+        }
+        return this.size;
+    }
+
+    public void incrementSize(int inc, int time) {
+        this.size += inc;
+        updateSize(this.size, time);
+    }
+
+    public void updateSize(int size, int time) {
+        this.size = size;
+        Bukkit.getWorld("world").getWorldBorder().setSize(size, time);
+        try {
+            statement.executeUpdate("UPDATE mc.size SET size=" + size);
+        } catch (SQLException e) {
+            Bukkit.broadcastMessage(e.toString());
+        } catch (Exception f) {
+            Bukkit.broadcastMessage("Something went wrong");
+        }
     }
 }
--- Worldborder/src/main/java/dev/nierot/worldborder/Event.java
+++ Worldborder/src/main/java/dev/nierot/worldborder/Event.java
@@ -3,26 +3,39 @@ package dev.nierot.worldborder;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.*;
+
+import java.sql.Statement;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public final class Event implements Listener {
+
+    Statement statement;
+    App app;
+
+    public Event(App app) {
+        this.app = app;
+        this.statement = app.getStatement();
+    }
+
 
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
         Bukkit.broadcastMessage(event.getPlayer().getName() + " is verslaafd aan minecraft");
     }
 
-    @EventHandler
-    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
-        String player = event.getPlayer().getName();
-        Bukkit.broadcastMessage("Dankzij " + player + " is de worldborder 1 block groter!");
-        Bukkit.getWorld("world").getWorldBorder().setSize(Bukkit.getWorld("world").getWorldBorder().getSize() + 1, 60);
-    }
+    // @EventHandler
+    // public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+    //     Player player = event.getPlayer();
+    //     Bukkit.broadcastMessage("Dankzij " + player.getName() + " is de worldborder 1 block groter!");
+    //     app.incrementSize(1, 100);
+    // }
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
         String player = event.getPlayer().getName();
         Bukkit.broadcastMessage("Dankzij " + player + " is de worldborder 1 block kleiner! Kutjoch");
-        Bukkit.getWorld("world").getWorldBorder().setSize(Bukkit.getWorld("world").getWorldBorder().getSize() - 1, 10);
+        app.incrementSize(-1, 10);
     }
 }
